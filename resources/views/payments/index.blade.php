1251b1f1800a5f0ef7f85a32c7b4caa905cf5040
shazim ali <shazimali03@gmail.com>
2026-07-25 13:16:21 +0500

    shorten Add Billing button text to Billing

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -222,7 +222,7 @@
                             <svg class="h-4 w-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                             </svg>
-                            Add Billing
+                            Billing
                         </a>
                     @endif
                 </div>
